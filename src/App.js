2f8c30b214f9da821b0c671cb14292d889771426
Bharat-Dua <bharatdua13@gmail.com>
2024-10-16 17:02:13 +0530

    emoji-search-app

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./App.css";
+import { emojiData } from "./emojiData";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [hoverName, setHoverName] = useState("");
+  const [hoverTimeOut, setHoverTimeOut] = useState(null);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleMouseEnter = (name) => {
+    if (hoverTimeOut) clearTimeout(hoverTimeOut);
+    setHoverName(name);
+  };
+  const handleMouseLeave = () => {
+    const timeout = setTimeout(() => {
+      setHoverName("");
+    }, 300);
+    setHoverTimeOut(timeout);
+  };
+  const filterEmojis = emojiData.filter((emoji) => {
+    const emojiName = emoji.name.toLowerCase();
+    return emojiName.includes(searchTerm.toLowerCase());
+  });
+  console.log(emojiData);
+  return (
+    <div className="App">
+      <h1>Search Emoji</h1>
+      <input
+        type="text"
+        placeholder="search emoji"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="emoji-container">
+        {filterEmojis.map((emoji) => (
+          <span
+            key={emoji.id}
+            onMouseEnter={() => handleMouseEnter(emoji.name)}
+            onMouseLeave={handleMouseLeave}
+            className="emoji"
+          >
+            {emoji.symbol}
+          </span>
+        ))}
+      </div>
+      {hoverName && <div className="hover-name">{hoverName}</div>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojiData.js
@@ -0,0 +1,302 @@
+export const emojiData = [
+  {
+    id: "1",
+    name: "Grinning Face",
+    symbol: "😀",
+  },
+  {
+    id: "2",
+    name: "Grinning Face with Big Eyes",
+    symbol: "😃",
+  },
+  {
+    id: "3",
+    name: "Grinning Face with Smiling Eyes",
+    symbol: "😄",
+  },
+  {
+    id: "4",
+    name: "Beaming Face with Smiling Eyes",
+    symbol: "😁",
+  },
+  {
+    id: "5",
+    name: "Grinning Squinting Face",
+    symbol: "😆",
+  },
+  {
+    id: "6",
+    name: "Grinning Face with Sweat",
+    symbol: "😅",
+  },
+  {
+    id: "7",
+    name: "Face with Tears of Joy",
+    symbol: "😂",
+  },
+  {
+    id: "8",
+    name: "Rolling on the Floor Laughing",
+    symbol: "🤣",
+  },
+  {
+    id: "9",
+    name: "Slightly Smiling Face",
+    symbol: "🙂",
+  },
+  {
+    id: "10",
+    name: "Upside-Down Face",
+    symbol: "🙃",
+  },
+  {
+    id: "11",
+    name: "Winking Face",
+    symbol: "😉",
+  },
+  {
+    id: "12",
+    name: "Smiling Face with Smiling Eyes",
+    symbol: "😊",
+  },
+  {
+    id: "13",
+    name: "Smiling Face with Halo",
+    symbol: "😇",
+  },
+  {
+    id: "14",
+    name: "Smiling Face with Hearts",
+    symbol: "🥰",
+  },
+  {
+    id: "15",
+    name: "Smiling Face with Heart-Eyes",
+    symbol: "😍",
+  },
+  {
+    id: "16",
+    name: "Star-Struck",
+    symbol: "🤩",
+  },
+  {
+    id: "17",
+    name: "Face Blowing a Kiss",
+    symbol: "😘",
+  },
+  {
+    id: "18",
+    name: "Kissing Face",
+    symbol: "😗",
+  },
+  {
+    id: "19",
+    name: "Smiling Face",
+    symbol: "☺️",
+  },
+  {
+    id: "20",
+    name: "Kissing Face with Closed Eyes",
+    symbol: "😚",
+  },
+  {
+    id: "21",
+    name: "Kissing Face with Smiling Eyes",
+    symbol: "😙",
+  },
+  {
+    id: "22",
+    name: "Smiling Face with Tear",
+    symbol: "🥲",
+  },
+  {
+    id: "23",
+    name: "Face Savoring Food",
+    symbol: "😋",
+  },
+  {
+    id: "24",
+    name: "Face with Tongue",
+    symbol: "😛",
+  },
+  {
+    id: "25",
+    name: "Winking Face with Tongue",
+    symbol: "😜",
+  },
+  {
+    id: "26",
+    name: "Zany Face",
+    symbol: "🤪",
+  },
+  {
+    id: "27",
+    name: "Squinting Face with Tongue",
+    symbol: "😝",
+  },
+  {
+    id: "28",
+    name: "Money-Mouth Face",
+    symbol: "🤑",
+  },
+  {
+    id: "29",
+    name: "Hugging Face",
+    symbol: "🤗",
+  },
+  {
+    id: "30",
+    name: "Face with Hand Over Mouth",
+    symbol: "🤭",
+  },
+  {
+    id: "31",
+    name: "Shushing Face",
+    symbol: "🤫",
+  },
+  {
+    id: "32",
+    name: "Thinking Face",
+    symbol: "🤔",
+  },
+  {
+    id: "33",
+    name: "Zipper-Mouth Face",
+    symbol: "🤐",
+  },
+  {
+    id: "34",
+    name: "Face with Raised Eyebrow",
+    symbol: "🤨",
+  },
+  {
+    id: "35",
+    name: "Neutral Face",
+    symbol: "😐",
+  },
+  {
+    id: "36",
+    name: "Expressionless Face",
+    symbol: "😑",
+  },
+  {
+    id: "37",
+    name: "Face Without Mouth",
+    symbol: "😶",
+  },
+  {
+    id: "38",
+    name: "Smirking Face",
+    symbol: "😏",
+  },
+  {
+    id: "39",
+    name: "Unamused Face",
+    symbol: "😒",
+  },
+  {
+    id: "40",
+    name: "Face with Rolling Eyes",
+    symbol: "🙄",
+  },
+  {
+    id: "41",
+    name: "Grimacing Face",
+    symbol: "😬",
+  },
+  {
+    id: "42",
+    name: "Lying Face",
+    symbol: "🤥",
+  },
+  {
+    id: "43",
+    name: "Relieved Face",
+    symbol: "😌",
+  },
+  {
+    id: "44",
+    name: "Pensive Face",
+    symbol: "😔",
+  },
+  {
+    id: "45",
+    name: "Sleepy Face",
+    symbol: "😪",
+  },
+  {
+    id: "46",
+    name: "Drooling Face",
+    symbol: "🤤",
+  },
+  {
+    id: "47",
+    name: "Sleeping Face",
+    symbol: "😴",
+  },
+  {
+    id: "48",
+    name: "Face with Medical Mask",
+    symbol: "😷",
+  },
+  {
+    id: "49",
+    name: "Face with Thermometer",
+    symbol: "🤒",
+  },
+  {
+    id: "50",
+    name: "Face with Head-Bandage",
+    symbol: "🤕",
+  },
+  {
+    id: "51",
+    name: "Nauseated Face",
+    symbol: "🤢",
+  },
+  {
+    id: "52",
+    name: "Face Vomiting",
+    symbol: "🤮",
+  },
+  {
+    id: "53",
+    name: "Sneezing Face",
+    symbol: "🤧",
+  },
+  {
+    id: "54",
+    name: "Hot Face",
+    symbol: "🥵",
+  },
+  {
+    id: "55",
+    name: "Cold Face",
+    symbol: "🥶",
+  },
+  {
+    id: "56",
+    name: "Woozy Face",
+    symbol: "🥴",
+  },
+  {
+    id: "57",
+    name: "Dizzy Face",
+    symbol: "😵",
+  },
+  {
+    id: "58",
+    name: "Exploding Head",
+    symbol: "🤯",
+  },
+  {
+    id: "59",
+    name: "Cowboy Hat Face",
+    symbol: "🤠",
+  },
+  {
+    id: "60",
+    name: "Partying Face",
+    symbol: "🥳",
+  },
+];
